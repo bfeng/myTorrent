9a6e6d2326e0a21d926c444a784485b3e14280f6
Bo Feng <bfeng1216@gmail.com>
2012-09-13 15:49:40 -0500

    add output method for the message

--- src/main/java/mytorrent/p2p/P2PProtocol.java
+++ src/main/java/mytorrent/p2p/P2PProtocol.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -45,5 +46,9 @@ public class P2PProtocol {
             Logger.getLogger(P2PProtocol.class.getName()).log(Level.SEVERE, null, ex);
             return new Message(Command.ERR, ex);
         }
+    }
+
+    public Message preparedOutput(OutputStream os) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
