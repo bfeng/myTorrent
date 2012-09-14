fd5e00e243975167b6ff17fa968ace0c2f0bbae1
Bo Feng <bfeng1216@gmail.com>
2012-09-14 00:12:07 -0500

    fix import

--- src/test/java/mytorrent/IndexServerTestCase.java
+++ src/test/java/mytorrent/IndexServerTestCase.java
@@ -1,10 +1,12 @@
 package mytorrent;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.After;
@@ -51,25 +53,23 @@ public class IndexServerTestCase {
 
     @Test
     public void testPing() {
-        for (int i = 0; i < 100; i++) {
-            try {
-                Socket sock = new Socket("localhost", 5700);
-                PrintWriter out = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
-                BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        try {
+            Socket sock = new Socket("localhost", 5700);
+            PrintWriter out = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 
-                out.println("ping");
-                out.flush();
+            out.println("ping");
+            out.flush();
 
-                assertEquals("ping", in.readLine());
+            assertEquals("ping", in.readLine());
 
-                in.close();
-                out.close();
-                sock.close();
+            in.close();
+            out.close();
+            sock.close();
 
-            } catch (Exception ex) {
-                Logger.getLogger(IndexServerTestCase.class.getName()).log(Level.SEVERE, null, ex);
-                fail(ex.getMessage());
-            }
+        } catch (Exception ex) {
+            Logger.getLogger(IndexServerTestCase.class.getName()).log(Level.SEVERE, null, ex);
+            fail(ex.getMessage());
         }
     }
 }
